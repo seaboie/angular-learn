d915ae06e53c2ced7496ea9675446a37d67375da
kritbovorn <kritbovorn@gmail.com>
2025-04-16 15:15:34 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tutorial';
-  city = 'San Francisco';
+  isLoggedIn = true;
+  isServerRunning = false;
+
+  operatingSystems = [{id: 'win', name: 'Windows'}, {id: 'osx', name: 'MacOS'}, {id: 'linux', name: 'Linux'}];
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
 }
