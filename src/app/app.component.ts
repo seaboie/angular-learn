78ce0d5d63168e1736ac422485da1630eb89d365
kritbovorn <kritbovorn@gmail.com>
2025-04-16 21:49:15 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from "./user/user.component";
+import { ChildComponent } from "./child/child.component";
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent],
+  imports: [UserComponent, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
+  items: string[] = [];
+  handleAddItem(item: string) {
+    this.items.push(item);
+  }
+
+  count: number = 0;
+  handleIncrement(item: number) {
+    this.count = item;
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css',
+})
+export class ChildComponent {
+  count: number = 0;
+  @Output() incrementCountEvent = new EventEmitter<number>();
+  onEmitIncrement() {
+    this.count++;
+    this.incrementCountEvent.emit(this.count);
+  }
+
+  @Output() addItemEvent = new EventEmitter<string>();
+  onEmitAddItem() {
+    this.addItemEvent.emit('🐢');
+  }
+}
