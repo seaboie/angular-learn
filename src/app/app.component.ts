fc7334400008f1a0fec9e93d2abbf7201b7f10f7
kritbovorn <kritbovorn@gmail.com>
2025-04-16 12:03:24 +0700

    basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tutorial';
+  city = 'San Francisco';
 }
