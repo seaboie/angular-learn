97893a4c1fe07adfd76d76db7dc59d09abda1d34
kritbovorn <kritbovorn@gmail.com>
2025-04-19 19:51:22 +0700

    create housing location component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
