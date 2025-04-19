12e79a090b16eb50eca7e95f60fb93f91bd62fb3
kritbovorn <kritbovorn@gmail.com>
2025-04-19 20:18:50 +0700

    Add @Input decorator to component

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../../interface/housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
 
 }
