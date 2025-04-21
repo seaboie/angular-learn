08e81f56033b1a7fcc8052c373924d59fc953afd
kritbovorn <kritbovorn@gmail.com>
2025-04-22 00:13:59 +0700

    reverse to standard code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tutorial';
-  city = 'San Francisco';
 }
