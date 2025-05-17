71f09384a943e1f4cca0648e08324ae24147d614
kritbovorn <kritbovorn@gmail.com>
2025-05-17 22:37:42 +0700

    finsih

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { CommonModule, NgComponentOutlet } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PostsListComponent } from './widgets/posts-list/posts-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule, PostsListComponent, NgComponentOutlet],
+  imports: [CommonModule, FormsModule, PostsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+
+  constructor(private viewContainer: ViewContainerRef) {}
   loadComponent() {
-    return PostsListComponent;
+    this.viewContainer.createComponent(PostsListComponent);
+  }
+
+  removeComponent() {
+    this.viewContainer.remove();
   }
 }
